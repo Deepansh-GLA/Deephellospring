4ab34e1bbc3f485561891cf3323e94561b65515f
Deepansh-GLA <deepansh.niranjan_cs21@gla.ac.in>
2025-03-01 14:51:48 +0530

    first commit

---
+++ First-Soring-App/src/main/java/com/example/First/Soring/App/FirstSoringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.First.Soring.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSoringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSoringAppApplication.class, args);
+	}
+
+}
---
+++ First-Soring-App/src/test/java/com/example/First/Soring/App/FirstSoringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.First.Soring.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSoringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
